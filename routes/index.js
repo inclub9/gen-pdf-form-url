760a892aba772835994768a8d56515ee04259133
inclub9 <inclub9@gmail.com>
2024-05-10 12:14:53 +0700

    first commit

---
+++ routes/index.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router();
+const puppeteer = require('puppeteer');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+router.get('/generate-pdf', async (req, res) => {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    console.log(req.body.url);
+    await page.goto(req.body.url);
+    // await page.waitForSelector('body', { timeout: 30000 });
+    // await page.waitForFunction(() => {
+    //   const text = document.querySelector('body').textContent;
+    //   return text.includes('How do I find');
+    // }, { timeout: 30000 });
+    sleep(5000)
+    const pdfBuffer = await page.pdf({ format: 'A4', landscape: false, scale: 1 });
+    await browser.close();
+
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Disposition', 'attachment; filename="page.pdf"');
+    res.send(pdfBuffer);
+  } catch (error) {
+    console.error('Error generating PDF:', error);
+    res.status(500).send('Error generating PDF');
+  }
+});
+
+
+module.exports = router;
