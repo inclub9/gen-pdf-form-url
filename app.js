e1babea3da3754ef2b3269236f7d9c5c3452f8ed
inclub9 <inclub9@gmail.com>
2024-05-10 12:47:11 +0700

    change port

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 10000;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
